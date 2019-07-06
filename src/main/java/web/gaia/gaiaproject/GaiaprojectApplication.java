385f66b06ed0e5314e9e7263c17ed46eebf49d19
干子雄 <g64848066@gmail.com>
2019-07-06 23:52:14 +0800

    初始化项目

---
+++ src/main/java/web/gaia/gaiaproject/GaiaprojectApplication.java
@@ -0,0 +1,13 @@
+package web.gaia.gaiaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaiaprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GaiaprojectApplication.class, args);
+    }
+
+}
