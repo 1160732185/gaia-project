d12c3496ad4318365faf0e057b327479f0d19af0
干子雄 <g64848066@gmail.com>
2019-07-31 16:57:31 +0800

    上个更新名字没改，为种族初始资源调整与bon板选择初步

---
+++ src/main/java/web/gaia/gaiaproject/model/TechTile.java
@@ -0,0 +1,22 @@
+package web.gaia.gaiaproject.model;
+
+public class TechTile {
+    String ttno;
+    String ttname;
+
+    public String getTtno() {
+        return ttno;
+    }
+
+    public void setTtno(String ttno) {
+        this.ttno = ttno;
+    }
+
+    public String getTtname() {
+        return ttname;
+    }
+
+    public void setTtname(String ttname) {
+        this.ttname = ttname;
+    }
+}
